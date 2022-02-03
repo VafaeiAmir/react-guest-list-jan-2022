abff77b580cedc600afeadd4e58b6391752171e0
AmirVafaei <amirvafaei.contact@gmail.com>
2022-02-03 21:19:06 +0100

    Add changes to GuestListApp.js

--- src/GuestListApp.js
+++ src/GuestListApp.js
@@ -1,17 +1,136 @@
 import './App.css';
-// import { css } from '@emotion/react';
+import { css } from '@emotion/react';
 import { useEffect, useState } from 'react';
 
 /** @jsxImportSource @emotion/react */
 
-const baseUrl = 'http://amirs-guest-list.herokuapp.com/guests/';
+const firstListStyle = css`
+  display: flex;
+  justify-content: center;
+`;
+const checkBoxStyle = css`
+  margin-top: auto;
+  margin-bottom: auto;
+`;
+const firstBodyTextStale = css`
+  display: flex;
+  justify-content: center;
+  background-color: lightsteelblue;
+  padding: 1rem;
+  font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+  font-size: 2rem;
+  color: black;
+`;
+const guestListStyle = css`
+  display: flex;
+  justify-content: center;
+  padding: 20px 20px;
+  background-color: lightsteelblue;
+  // position: fixed;
+  // left: 100px;
+`;
+const removeBoxStyle = css`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  background-color: red;
+  border: none;
+  color: white;
+  height: 15px;
+  font-size: 0.5rem;
+  margin-left: 5px;
+  margin-top: auto;
+  :hover {
+    font-size: 0.7rem;
+    transition: font-size 0.2s ease;
+  }
+`;
+const removeAllButtonStyle = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: lightgrey;
+  transition: background-color 0.2s ease;
+  color: black;
+  text-align: center;
+  font-family: serif;
+  vertical-align: middle;
+  height: 30px;
+  width: 130px;
+  font-size: 1rem;
+  letter-spacing: 0.03rem;
+  line-height: 1.269rem;
+  margin-right: auto;
+  margin-left: auto;
+  margin-top: auto;
+  margin-bottom: auto;
+  :hover {
+    background-color: lightsalmon;
+    font-size: 1.1rem;
+    transition: font-size 0.2s ease;
+  }
+`;
+const addButtonStyle = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: lightgrey;
+  transition: background-color 0.2s ease;
+  color: black;
+  font-family: serif;
+  text-align: center;
+  vertical-align: middle;
+  width: 130px;
+  height: 30px;
+  font-size: 1rem;
+  letter-spacing: 0.018rem;
+  line-height: 1.269rem;
+  margin-right: auto;
+  margin-left: auto;
+  margin-top: auto;
+  margin-bottom: auto;
+  :hover {
+    background-color: lightseagreen;
+    font-size: 1.1rem;
+    transition: font-size 0.2s ease;
+  }
+`;
+const nameFieldStyle = css`
+  display: flexbox;
+  justify-content: center;
+  // border-style: none;
+  height: 30px;
+  margin: 10px;
+  margin-right: 5rem;
+`;
+const inputStyle = css`
+  display: grid;
+  justify-content: center;
+  // margin: 0 30rem;
+  // justify-content: center;
+  // position: absolute;
+  // left: 500px;
+`;
+const guestInListStyle = css`
+  display: flex;
+  justify-content: center;
+  // min-width: 400px;
+`;
+const textAttStyle = css`
+  padding-left: 0.6rem;
+  color: gray;
+`;
+const pleaseTextStyle = css`
+  margin-right: 2rem;
+`;
+const baseUrl = 'http://amirs-guest-list.herokuapp.com';
 
 // item of each guest
 function Guest(props) {
   const [attending, setAttending] = useState(props.attending);
 
   async function updateAttending(id) {
-    const response = await fetch(`${baseUrl}${id}`, {
+    const response = await fetch(`${baseUrl}/guests/${id}`, {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
@@ -24,8 +143,9 @@ function Guest(props) {
   }
 
   return (
-    <li key={props.firstName} data-test-id="guest">
+    <li css={firstListStyle} key={props.firstName} data-test-id="guest">
       <input
+        css={checkBoxStyle}
         aria-label="attending"
         type="checkbox"
         checked={attending}
@@ -39,7 +159,11 @@ function Guest(props) {
         Name: {props.firstName} {props.lastName}
         <span> </span>
       </p>
-      {attending ? <p> attending</p> : <p>not attending</p>}
+      {attending ? (
+        <p css={textAttStyle}> attending</p>
+      ) : (
+        <p css={textAttStyle}>not attending</p>
+      )}
     </li>
   );
 }
@@ -52,7 +176,7 @@ function All() {
 
   // Add user
   async function createUser(input1, input2) {
-    const response = await fetch(`${baseUrl}/`, {
+    const response = await fetch(`${baseUrl}/guests`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -71,7 +195,7 @@ function All() {
   // Remove guest
 
   async function handleRemove(id) {
-    const response = await fetch(`${baseUrl}${id}`, {
+    const response = await fetch(`${baseUrl}/guests/${id}`, {
       method: 'DELETE',
     });
     const deletedGuest = await response.json();
@@ -91,7 +215,7 @@ function All() {
 
   useEffect(() => {
     async function getAllGuests() {
-      const response = await fetch(`${baseUrl}/`);
+      const response = await fetch(`${baseUrl}/guests`);
       const allGuests = await response.json();
       console.log(allGuests);
       setGuestsList(allGuests);
@@ -105,12 +229,14 @@ function All() {
   const disabled = loading ? true : false;
   return (
     <div className="App">
-      <h1> Guest List </h1>
+      <h1 css={firstBodyTextStale}> Bienvenido al restaurante El Gaucho</h1>
+      <h1 css={firstBodyTextStale}>Lista de reservas</h1>
       <div>
-        <div>
+        <div css={inputStyle}>
           <label>
             First Name
             <input
+              css={nameFieldStyle}
               value={firstName}
               onChange={(event) => {
                 aetFirstName(event.currentTarget.value);
@@ -122,6 +248,7 @@ function All() {
           <label>
             Last Name
             <input
+              css={nameFieldStyle}
               value={lastName}
               onChange={(event) => {
                 setLastName(event.currentTarget.value);
@@ -138,6 +265,7 @@ function All() {
           </label>
           <br />
           <button
+            css={addButtonStyle}
             onClick={() => {
               createUser(firstName, lastName).catch((error) => {
                 console.error('Error:', error);
@@ -147,6 +275,7 @@ function All() {
             Add guest
           </button>
           <button
+            css={removeAllButtonStyle}
             onClick={() => {
               handleRemoveGuests().catch((error) => {
                 console.error('Error:', error);
@@ -156,16 +285,16 @@ function All() {
             Remove guests
           </button>
         </div>
-        <div>
+        <div css={guestListStyle}>
           {loading === true ? (
             <p>Loading...</p>
           ) : (
             <div>
-              <ul>
-                {guestsList.length === 0 && <p>Please add a guest!</p>}
+              <ul css={pleaseTextStyle}>
+                {guestsList.length === 0 && <p>Please add a guest! </p>}
                 {guestsList.map((e) => {
                   return (
-                    <div key={e.id + e.firstName}>
+                    <div key={e.id + e.firstName} css={guestInListStyle}>
                       <Guest
                         key={e.id + e.firstName + e.lastName}
                         firstName={e.firstName}
@@ -174,6 +303,7 @@ function All() {
                         id={e.id}
                       />
                       <button
+                        css={removeBoxStyle}
                         onClick={() => {
                           handleRemove(e.id).catch((error) => {
                             console.error('Error:', error);
